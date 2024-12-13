14c239f6419209d4598f3a8b4ce2b61a056e45fc
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-13 08:50:20 +0100

    Add project files.

---
+++ Uppgift333/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+  
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Ange ett heltal: ");
+            int number = int.Parse(Console.ReadLine());
+            sum += number;
+        }
+
+        Console.WriteLine("Summan av de inmatade talen är: " + sum);
+    }
+}
+
+
